7b1518175c76432ab26b151fc8074a14f4abc951
sivan18-meet <sbmislovely@gmail.com>
2017-11-12 19:05:48 +0200

    adding lab3 changes and lab4

--- lab 3/lab3.py
+++ lab 3/lab3.py
@@ -1,7 +1,29 @@
-import turtle 
-turtle.register_shape("pentagon",((0,0), (50,0), (50,-50), (25,-75), (0,-50)))
-turtle.shape("pentagon")
+import turtle
+#turtle.speed(100)
+#turtle.pensize(10)
+#a=20
+#for i in range (4):
+#	turtle.forward(100)
+#	turtle.left(90)
+#	turtle.forward(100)
+#	turtle.left(90)
 
+	
+
+#turtle.register_shape("pentagon",((0,0), (50,0), (50,-50), (25,-75), (0,-50)))
+#turtle.shape("pentagon")
+
+#angle=5
+#for i in range(361):
+#	turtle.forward(100)
+#	turtle.right(50)
+#	turtle.forward(120)
+#	turtle.left(10)
+#	turtle.forward(30)
+#	turtle.home()
+#	turtle.right(angle)
+#	angle=angle + 1	
+
+turtle.addshape("pizzatamara.gif")
+turtle.shape('pizzatamara.gif')
 turtle.mainloop()
-
-
---
+++ lab 4.py
@@ -0,0 +1,12 @@
+class Animal(object):
+	def __init__(self,sound,name,age,favorite_color):
+	self.sound = sound
+	self.name = name
+	self.age = age
+	self.favorite_color
+def eat(self,food):
+	print("yummy!!" + self.name + "is eating" + food)
+def description(self)
+	print(self.name + "is" + self.age + "years old and loves the color" + self.favorite_color)
+j= Animal(Roar, Lion , 30, Black)
+print=j.eat("pizza")
